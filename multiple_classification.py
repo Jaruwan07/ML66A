6986f39f8a8fc6c184db8d9a249ee1690f47181e
jahlnwzaa_eiei <1166105050486@mail.rmutt.ac.th>
2026-02-25 16:49:46 +0700

    Update multiple_classification.py

--- multiple_classification.py
+++ multiple_classification.py
@@ -107,21 +107,9 @@ if(selected == 'Loan'):
 if(selected == 'BMI'):
     st.title('bmi Classification')
     
-    person_age = st.text_input('person_age')
-    person_gender = st.selectbox('person_gender', gender_map)
-    person_education = st.selectbox('person_education', education_map)
-    person_income = st.text_input('person_income') 
-    person_emp_exp = st.text_input('person_emp_exp')
-    person_home_ownership = st.selectbox('person_home_ownership', home_map)
-    loan_amnt = st.text_input('loan_amnt')
-    loan_intent = st.selectbox('loan_intent', intent_map)
-    loan_int_rate = st.text_input('loan_int_rate')
-    loan_percent_income = st.text_input('loan_percent_income')
-    cb_person_cred_hist_length = st.text_input('cb_person_cred_hist_length')
-    credit_score = st.text_input('credit_score')
-    previous_loan_defaults_on_file = st.selectbox(
-        'previous_loan_defaults_on_file',
-        default_map)
+    gender = st.selectbox('Gender', ['Male', 'Female'])
+    height = st.text_input('Height (cm)')
+    weight = st.text_input('Weight (kg)')
     
     bmi_prediction = ''
     
@@ -177,6 +165,7 @@ if(selected == 'Riding'):
           
 
     st.success(Riding_prediction)
+
 
 
 
